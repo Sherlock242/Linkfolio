dff8bbdd7761203c4113f38a73353097856ba381
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-09-29 12:59:24 +0000

    I added icon.png make sure you use it as a favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="/icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       </head>
