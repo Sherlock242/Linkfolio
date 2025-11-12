72ee593feb46748c21476bd49e686147fbcca402
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-11-12 18:22:18 +0000

    I want to add ads in my site so these are the script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: 'LinkFolio',
@@ -22,6 +23,8 @@ export default function RootLayout({
       <body className="font-body antialiased bg-background text-foreground">
         {children}
         <Toaster />
+        <Script type='text/javascript' src='//certainwolveshonestly.com/c2/e3/2a/c2e32af9398348a7c7abfb3b5f4985df.js' />
+        <Script type='text/javascript' src='//certainwolveshonestly.com/b8/17/3b/b8173b354c2214ec5a8c4a1b64c0e6b8.js' />
       </body>
     </html>
   );
